ba5c6d73049b1ba61de61647055ce06d0ccfcef7
youssefsameh55 <128090819+youssefsameh55@users.noreply.github.com>
2025-05-06 02:16:11 +0300

    path

--- src/app/404.js
+++ src/app/404.js
@@ -1,9 +1,21 @@
 "use client";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
 export default function NotFoundPage() {
+  const [pathname, setPathname] = useState("");
+  const path = usePathname();
+
+  useEffect(() => {
+    setPathname(path);
+  }, [path]);
+
   return (
-    <div style={{ textAlign: 'center', marginTop: 80 }}>
+    <div style={{ textAlign: "center", marginTop: 80 }}>
       <h1 style={{ fontSize: 36, fontWeight: 700 }}>404 - Page Not Found</h1>
-      <p style={{ color: '#888', marginTop: 16 }}>Sorry, the page you are looking for does not exist.</p>
+      <p style={{ color: "#888", marginTop: 16 }}>
+        Sorry, the page {pathname} you are looking for does not exist.
+      </p>
     </div>
   );
 }
